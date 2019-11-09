56587c67faa01cb00756a674ed01b701705d0a1d
Ryan Challen <ryanchallen@outlook.com>
2019-11-09 14:33:32 +1000

    Added basic options for user

---
+++ calculator.rb
@@ -0,0 +1,27 @@
+# Basic Calculator app
+
+=begin 
+
+    Requirements:
+
+    1. need to be able to print options
+    2. need to be able to quit
+    3. needs to take to two values and:
+        sum
+        subtract
+        divide
+        multiply
+        square
+=end
+
+loop do 
+
+    puts "options"
+    puts "1. Sum"
+    puts "2. Subtract"
+    puts "3. Divide"
+    puts "4. Multiply"
+    puts "5. Square"
+    puts "6. Quit"
+
+end
